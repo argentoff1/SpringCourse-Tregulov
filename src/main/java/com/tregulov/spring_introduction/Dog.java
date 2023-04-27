8a166355110cb6b68e65f702151fd812046a1856
argentoff1 <mparinos56@gmail.com>
2023-04-27 08:58:51 +0300

    Bean scope рассмотрен

--- src/main/java/com/tregulov/spring_introduction/Dog.java
+++ src/main/java/com/tregulov/spring_introduction/Dog.java
@@ -2,11 +2,19 @@ package com.tregulov.spring_introduction;
 
 public class Dog implements Pet {
     public Dog() {
-        System.out.println("Бин собаки создан");
+        System.out.println("Dog bean is created");
     }
 
     @Override
     public void say() {
         System.out.println("Гав-гав");
+    }
+
+    public void init() {
+        System.out.println("Class Dog: init method");
+    }
+
+    public void destroy() {
+        System.out.println("Class Dog: destroy method");
     }
 }
