a60637452db650689f074252ca93f0d3de5103b5
argentoff1 <mparinos56@gmail.com>
2023-04-28 15:20:13 +0300

    Комбинации Pointcut разобраны

--- src/main/java/com/tregulov/aop/Test1.java
+++ src/main/java/com/tregulov/aop/Test1.java
@@ -8,10 +8,14 @@ public class Test1 {
                 new AnnotationConfigApplicationContext(MyConfig.class);
 
         UniversityLibrary universityLibrary = context.getBean("universityLibrary", UniversityLibrary.class);
-        Book book = context.getBean("book", Book.class);
-        universityLibrary.getBook(book);
+        //Book book = context.getBean("book", Book.class);
+        universityLibrary.getBook();
 
-        //universityLibrary.getMagazine();
+        //System.out.println();
+
+        universityLibrary.returnMagazine();
+
+        universityLibrary.addBook();
 
         /*universityLibrary.returnBook();
         universityLibrary.getMagazine();
--- src/main/java/com/tregulov/aop/UniversityLibrary.java
+++ src/main/java/com/tregulov/aop/UniversityLibrary.java
@@ -4,16 +4,27 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UniversityLibrary extends AbstractLibrary {
-
-    public void getBook(Book book) {
-        System.out.println("Берем книгу из University Library " + book.getName());
+    public void getBook() {
+        System.out.println("Берем книгу из University Library");
     }
 
-    /*public void returnBook() {
+    public void returnBook() {
         System.out.println("Мы возвращаем книгу в University Library");
-    }*/
+    }
 
     public void getMagazine() {
         System.out.println("Берем журнал из University Library");
+    }
+
+    public void returnMagazine() {
+        System.out.println("Мы возвращаем журнал в University Library");
+    }
+
+    public void addBook() {
+        System.out.println("Мы добавляем книгу в University Library");
+    }
+
+    public void addMagazine() {
+        System.out.println("Мы добавляем журнал в University Library");
     }
 }
